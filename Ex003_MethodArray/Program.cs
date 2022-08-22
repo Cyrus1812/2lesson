23fe9a8cf51c5a35bb4108d2845186c61d5b7c92
MAX <maksimdemidovru@mail.ru>
2022-08-22 15:45:28 +0300

    add Ex003 поиск индекса

---
+++ Ex003_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,45,22,48,96,13,75,56};
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
